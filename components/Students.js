4e65a40b26f68b05d12d757788dee2f164883192
amrin2509 <sorcheatamrin954@gmail.com>
2026-05-26 16:41:59 +0700

    update data

--- components/Students.js
+++ components/Students.js
@@ -2,7 +2,7 @@
 const API =
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://school-api.vercel.app";
+    : "https://school-web-jet.vercel.app";
 
 fetch(`${API}/api/students/total`)
   .then((r) => r.json())
